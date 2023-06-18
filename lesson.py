7ea1e6ec83e6d9d321a1b6f8627bb2ccec35e9f3
ryota-09 <n.ryo950911@gmail.com>
2023-06-18 19:51:22 +0900

    first commit

---
+++ lesson.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+credits_df = pd.read_csv("./data/credits.csv")
+movies_df = pd.read_csv("./data/movies.csv")
+
+pd.set_option("display.max_columns", None)
+pd.set_option("display.max_rows", None)
+
+
